77e7353dcc7aaec6075d5e7014d6552412f30f03
Nick Avramoussis <4256455+Idclip@users.noreply.github.com>
2020-05-15 16:38:30 +0100

    wip

--- ci/install_llvm.sh
+++ ci/install_llvm.sh
@@ -3,5 +3,6 @@
 set -ex
 
 wget -O llvm-$LLVM_VERSION.tar.xz https://releases.llvm.org/$LLVM_VERSION/clang+llvm-$LLVM_VERSION-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+echo "Installing LLVM $LLVM_VERSION -> $LLVM_ROOT_DIR"
 tar xf llvm-$LLVM_VERSION.tar.xz --directory $LLVM_ROOT_DIR --strip 1
 rm -f llvm-$LLVM_VERSION.tar.xz
