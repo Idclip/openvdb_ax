98040296b69ccb9a061bab10c1fb4c07529edab2
Nick Avramoussis <4256455+Idclip@users.noreply.github.com>
2020-05-18 11:49:55 +0100

    wip

--- ci/install_llvm.sh
+++ ci/install_llvm.sh
@@ -2,6 +2,17 @@
 
 set -ex
 
+LLVM_VERSION=$1
+LLVM_ROOT_DIR=$2
+if [ -z $LLVM_VERSION ]; then
+    echo "No LLVM version provided for LLVM installation"
+    exit -1
+fi
+if [ -z $LLVM_ROOT_DIR ]; then
+    echo "No installation directory provided for LLVM installation"
+    exit -1
+fi
+
 wget -O llvm-$LLVM_VERSION.tar.xz https://releases.llvm.org/$LLVM_VERSION/clang+llvm-$LLVM_VERSION-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 
 echo "Installing LLVM $LLVM_VERSION -> $LLVM_ROOT_DIR"
